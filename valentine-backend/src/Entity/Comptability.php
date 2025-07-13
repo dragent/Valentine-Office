a51c48b97b654f95203d56d6be785c1dc484e3c8
dragent <alexiscau@gmail.com>
2025-07-13 12:01:31 +0200

    feat ( entity )  : add of comptability

---
+++ valentine-backend/src/Entity/Comptability.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComptabilityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ComptabilityRepository::class)]
+class Comptability
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+}
