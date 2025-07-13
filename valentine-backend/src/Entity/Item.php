d78f37909bdda9494eedf1a968a9c8acc25b80c3
dragent <alexiscau@gmail.com>
2025-07-13 15:49:48 +0200

    feat ( backend ) : create  entity  : - Armes - Item

---
+++ valentine-backend/src/Entity/Item.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ItemRepository::class)]
+class Item
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $isLegal = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isLegal(): ?bool
+    {
+        return $this->isLegal;
+    }
+
+    public function setIsLegal(bool $isLegal): static
+    {
+        $this->isLegal = $isLegal;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
